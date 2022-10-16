4b57c244211b4f6d4e135ade7eb6ead90def3109
Yash Walia <48391286+ywalia01@users.noreply.github.com>
2022-10-17 01:18:45 +0530

    rm warnings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./fonts/Termina-W05-Bold.ttf";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
