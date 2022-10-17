4dfdc59b49d944b40e7f51174166041a1098618d
Yash Walia <48391286+ywalia01@users.noreply.github.com>
2022-10-18 00:19:50 +0530

    fix className warning

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div class="flex justify-center items-start h-screen w-full text-white">
-      <p class="mt-10">Home Page</p>
+    <div className="flex justify-center items-start h-screen w-full text-white">
+      <p className="mt-10">Home Page</p>
     </div>
   );
 };
